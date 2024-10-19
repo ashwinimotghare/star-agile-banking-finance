c0c6a28f5a41eb5abfaa4bffc6649df097c94246
ashwinimotghare <ashwini.motghare@gmail.com>
2024-10-20 00:42:18 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "test-server" {
   ami = "ami-04a37924ffe27da53"
   instance_type = "t2.micro"
   key_name = "LinuxVM"
-  vpc_security_group_ids = ["sg-0074f9b186590ca2b"]
+  vpc_security_group_ids = ["sg-0b36772691b65fcc7"]
   connection {
      type = "ssh"
      user = "ec2-user"
